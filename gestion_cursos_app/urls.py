5161d1ad6ae9e8f59378658f961eeca6261ce422
Álvaro Falcón Casas <alvarofalconeti@gmail.com>
2024-12-17 22:11:44 +0100

    No me funciona nada en mi ordenador personal

--- gestion_cursos_app/urls.py
+++ gestion_cursos_app/urls.py
@@ -4,4 +4,15 @@ from . import views
 urlpatterns = [
     path('curso/', views.listar_cursos, name='listar_cursos'),
     path('curso/crear/', views.crear_curso, name='crear_curso'),
+    path('curso/editar/<int:pk>/', views.editar_curso, name='editar_curso'),
+    path('curso/eliminar/<int:pk>/', views.eliminar_curso, name='eliminar_curso'),
+    
+    path('estudiante/', views.listar_estudiantes, name='listar_estudiantes'),
+    path('estudiante/crear/', views.crear_estudiante, name='crear_estudiante'),
+    path('estudiante/editar/<int:pk>/', views.editar_estudiante, name='editar_estudiante'),
+    path('estudiante/eliminar/<int:pk>/', views.eliminar_estudiante, name='eliminar_estudiante'),
+
+    path('inscritos/', views.listar_inscripciones, name='listar_inscripciones'),
+    path('inscritos/new/', views.crear_inscripcion, name='crear_inscripcion'),
+    path('inscritos/del/<int:pk>/', views.eliminar_inscripcion, name='eliminar_inscripcion'),
 ]
--- gestion_cursos_app/views.py
+++ gestion_cursos_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Curso
-from .forms import curso_form
+from .models import Curso, Estudiante, Inscripcion
+from .forms import curso_form, estudiante_form, inscripcion_form
 
 # Vista para listar todos los cursos
 def listar_cursos(request):
@@ -18,3 +18,77 @@ def crear_curso(request):
     else:
         form = curso_form()
     return render(request, 'cursos/crear_curso.html', {'form': form})
+
+# Vista para editar un curso
+def editar_curso(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    if request.method == 'POST':
+        form = curso_form(request.POST, instance=curso)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_cursos')
+    else:
+        form = curso_form(instance=curso)
+    return render(request, 'cursos/editar_curso.html', {'form': form})
+
+# Vista para eliminar un curso
+def eliminar_curso(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    curso.delete()
+    return redirect('listar_cursos')
+
+#Vista para crear un nuevo estudiante
+def crear_estudiante(request):
+    if request.method == 'POST':
+        form = estudiante_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_estudiantes')
+    else:
+        form = estudiante_form()
+    return render(request, 'estudiantes/crear_estudiante.html', {'form': form})
+
+#Vista para listar todos los estudiantes
+def listar_estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'gestion_cursos_app/estudiantes/listar_estudiantes.html', {'estudiantes': estudiantes})
+
+#Vista para editar un estudiante
+def editar_estudiante(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    if request.method == 'POST':
+        form = estudiante_form(request.POST, instance=estudiante)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_estudiantes')
+    else:
+        form = estudiante_form(instance=estudiante)
+    return render(request, 'estudiantes/editar_estudiante.html', {'form': form})
+
+#Vista para eliminar un estudiante
+def eliminar_estudiante(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    estudiante.delete()
+    return redirect('listar_estudiantes')
+
+#Vista para crear una inscripcion
+def crear_inscripcion(request):
+    if request.method == 'POST':
+        form = inscripcion_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_inscripciones')
+    else:
+        form = inscripcion_form()
+    return render(request, 'inscripciones/crear_inscripcion.html', {'form': form})
+
+#Vista para listar todas las inscripciones
+def listar_inscripciones(request):
+    inscripciones = Inscripcion.objects.all()
+    return render(request, 'gestion_cursos_app/inscripciones/listar_inscripciones.html', {'inscripciones': inscripciones})
+
+#Vista para eliminar una inscripcion
+def eliminar_inscripcion(request, pk):
+    inscripcion = get_object_or_404(Inscripcion, pk=pk)
+    inscripcion.delete()
+    return redirect('listar_inscripciones')
